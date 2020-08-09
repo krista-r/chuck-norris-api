e7d37047b157bf64d98754cdb97f5a3038c0bc0c
Krista Rowland <krista21rowland@gmail.com>
2020-08-09 11:58:26 +0200

    updating fetchData for usability
    changing structure of fetchData() to be resused with setInterval

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,12 @@
   <div id="app">
     <header>
       <h1>Chuck Norris Joke</h1>
-      <button @click="fetchData">Fetch Jokes</button> 
+      <button @click="fetchData(10)">Fetch Jokes</button>
+     
+      <button @click="fetchRandom">
+        <span v-if="!currentState">Off</span>
+        <span v-else>On</span>
+      </button> 
     </header>
 
     <div class="lists-wrapper">
@@ -32,17 +37,37 @@ export default {
   data ()
   {
     return {
-      data: []
+      data: [],
+      currentState: false,
+      timer: null
     }
   },
 
   methods: {
-    fetchData()
+    fetchRandom()
     {
-      const baseURL = 'http://api.icndb.com/jokes/random/10'
+      this.currentState = !this.currentState;
+
+      if( !this.timer )
+      {
+        this.timer = setInterval(() => this.fetchData(1), 5000);
+      }else{
+        clearInterval(this.timer);
+        this.timer = null;
+      }
+    },
+
+    fetchData(n)
+    {
+      const baseURL = 'http://api.icndb.com/jokes/random/' + n
       this.$http.get(baseURL)
       .then((results) =>{
-        this.data = results.data.value;
+        const response = results.data.value
+        if(response.length === 1){
+          this.$store.state.favouritedJoke.push(response[0].joke);
+        }else{
+          this.data = response;
+        }
       })
     }
   }
